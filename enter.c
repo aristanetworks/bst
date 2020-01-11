f00478f76dd87f3f84bbd7ba8a0e470f1256a7cb
Andrew Nelson <andrew2nelson@arista.com>
2020-01-11 21:56:03 +0000

    b5-enter: write unit tests to cover exit status
    I found we were originally handling signals incorrectly. This patch also
    contains a fix for b5-enter to resolve it.
    
    Change-Id: I238fa872ae1e7fe5df9741301b5ca0cc1bf0c612

--- enter.c
+++ enter.c
@@ -107,7 +107,11 @@ int enter(const struct entry_settings *opts)
 		if (TEMP_FAILURE_RETRY(waitpid(pid, &status, 0)) == -1) {
 			err(1, "waitpid");
 		}
-		return WEXITSTATUS(status) | WTERMSIG(status) << 7;
+
+		if (WIFEXITED(status)) {
+			return WEXITSTATUS(status);
+		}
+		return WTERMSIG(status) | 1 << 7;
 	}
 
 	if (opts->user) {
