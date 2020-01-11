ec799b270e82ad650297a72cb41ac77042cacf45
Andrew Nelson <andrew2nelson@arista.com>
2020-01-11 03:00:21 +0000

    b5-enter needs to ensure target directories exist before mounting
    During image building, we will often mount to places like
    /src/github.com/foo/bar inside the image, which almost never actually
    exist in the image. With this patch, b5-enter creates these directories
    so that it can mount to them.
    
    This is probably not a good idea, because it actually creates
    directories in the floor - it would be better to first mount /src
    to a tmpfs and then create directories in the tmpfs that can
    hold the source mount. I will come back to this later, but I'm
    punting it for now in order to get unblocked on image building.
    
    Change-Id: I01243a0d290fca45cc4751f336486d85a1729dd5

--- mount.c
+++ mount.c
@@ -2,6 +2,7 @@
    Arista Networks, Inc. Confidential and Proprietary. */
 
 #include <err.h>
+#include <errno.h>
 #include <limits.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -25,6 +26,23 @@ static int cmpflags(const void *lhs, const void *rhs)
 	const char *flagname = lhs;
 	const struct mntflag *flag = rhs;
 	return strcmp(flagname, flag->name);
+}
+
+int mkdir_recursive(const char *dir, int mode)
+{
+	char tmp[PATH_MAX];
+	if (snprintf(tmp, PATH_MAX, "%s", dir) >= PATH_MAX) {
+		errx(1, "mkdir_recursive: path larger than PATH_MAX.");
+	}
+	for (char *p = tmp + 1; *p; p++) {
+		if (*p != '/') { continue; }
+		*p = 0;
+		if (mkdir(tmp, mode) == -1 && errno != EEXIST) {
+			return -1;
+		}
+		*p = '/';
+	}
+	return mkdir(tmp, mode);
 }
 
 /* update_mount_flags_and_options updates in-place its own input by
@@ -150,6 +168,10 @@ void mount_entries(const char *root, const struct mount_entry *mounts, size_t nm
 			errx(1, "mount_entries: target \"%s\" must be an absolute path.", mnt->target);
 		}
 		const char *target = makepath("%s%s", root, mnt->target);
+
+		if (mkdir_recursive(target, 0777) == -1 && errno != EEXIST) {
+			err(1, "mount: mkdir_recursive(\"%s\", %o)", target, 0777);
+		}
 
 		if (mount(mnt->source, target, mnt->type, flags, mnt->options) == -1) {
 			err(1, "mount_entries: mount(\"%s\", \"%s\", \"%s\", %lu, \"%s\")",
