aristanetworks/bst

commit 79f40f63650955153b60e2251f181aa630e8bf1b

sig: make PDEATHSIG handling non-racy
The way we've used PDEATHSIG so far was to call it in the child
immediately after fork(). This is however inherently racy, because
it is absolutely possible for the parent to get SIGKILL'ed after the
fork, but before the child gets to call prctl.

This fixes the race by introducing a new function, sig_setpdeathsig.

The function takes the kill signal to pass to prctl PR_SET_PDEATHSIG, as
well as the expected parent pid. The way to use this function is as
follows:

    pid_t ppid = getpid();
    pid_t pid = fork();
    if (pid == -1) {
        err(1, "fork");
    }
    if (!pid) {
        // in the child
        sig_setpdeathsig(SIGKILL, ppid);
    }

sig_setpdeathsig then takes care of calling prctl(PR_SET_PDEATHSIG, signo),
and checks whether the current ppid is the same as the original ppid. If
it isn't, it means the parent died and the child got reparented to the
nearest subreaper, and the function calls raise(signo).

enter.c
@@ -325,6 +325,11 @@ int enter(struct entry_settings *opts)
 		err(1, "socketpair");
 	}
 
+	/* Note that we need to get the parent pid here, as calling getppid()
+	   post-fork will not catch conditions where the parent gets SIGKILL'ed
+	   and the child ends up being reparented. */
+	pid_t expected_ppid = getpid();
+
 	/* You can't "really" unshare the PID namespace of a running process
 	   without forking, since for process hierarchy reasons only the next
 	   child process enters the namespace as init and subsequent calls to
@@ -461,9 +466,7 @@ int enter(struct entry_settings *opts)
 	   dies from uncatcheable signals. Or at least, we could, but this makes us
 	   leaky by default which isn't great, and the obvious workaround to
 	   daemonize the process tree is to just nohup bst. */
-	if (prctl(PR_SET_PDEATHSIG, SIGKILL) == -1) {
-		err(1, "prctl PR_SET_PDEATHSIG");
-	}
+	sig_setpdeathsig(SIGKILL, expected_ppid);
 
 	outer_helper_sync(&outer_helper);
 

outer.c
@@ -16,7 +16,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mount.h>
-#include <sys/prctl.h>
 #include <sys/epoll.h>
 #include <unistd.h>
 
@@ -24,6 +23,7 @@
 #include "enter.h"
 #include "outer.h"
 #include "path.h"
+#include "sig.h"
 #include "userns.h"
 #include "util.h"
 #include "fd.h"
@@ -314,6 +314,11 @@ void outer_helper_spawn(struct outer_helper *helper)
 		err(1, "outer_helper: socketpair");
 	}
 
+	/* Note that we need to get the parent pid here, as calling getppid()
+	   post-fork will not catch conditions where the parent gets SIGKILL'ed
+	   and the child ends up being reparented. */
+	pid_t expected_ppid = getpid();
+
 	pid_t pid = fork();
 	if (pid == -1) {
 		err(1, "outer_helper: fork");
@@ -325,9 +330,7 @@ void outer_helper_spawn(struct outer_helper *helper)
 		helper->fd  = fdpair[SOCKET_PARENT];
 		return;
 	}
-	if (prctl(PR_SET_PDEATHSIG, SIGKILL) == -1) {
-		err(1, "prctl PR_SET_PDEATHSIG");
-	}
+	sig_setpdeathsig(SIGKILL, expected_ppid);
 
 	sigset_t mask;
 	sigemptyset(&mask);

sig.c
@@ -8,6 +8,7 @@
 #include <errno.h>
 #include <stddef.h>
 #include <sys/epoll.h>
+#include <sys/prctl.h>
 #include <sys/signalfd.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -73,5 +74,18 @@ void sig_setup(int epollfd, const sigset_t *set, pid_t helper_pid, epoll_handler
 
 	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sigfd, &event) == -1) {
 		err(1, "epoll_ctl_add signalfd");
+	}
+}
+
+void sig_setpdeathsig(int signo, pid_t expected_ppid)
+{
+	if (prctl(PR_SET_PDEATHSIG, signo) == -1) {
+		err(1, "prctl PR_SET_PDEATHSIG");
+	}
+	if (getppid() != expected_ppid) {
+		/* The parent process died unexpectedly and we got reparented to the
+		   nearest subreaper. We won't get killed by the kernel anymore, because
+		   our new parent might be long lived, so just do it ourselves. */
+		raise(signo);
 	}
 }

sig.h
@@ -51,5 +51,6 @@ void sig_wait(const sigset_t *set, siginfo_t *info);
 void sig_forward(const siginfo_t *info, pid_t pid);
 void sig_read(int sigfd, siginfo_t *info);
 void sig_setup(int epollfd, const sigset_t *set, pid_t helper_pid, epoll_handler_fn *fn);
+void sig_setpdeathsig(int signo, pid_t expected_ppid);
 
 #endif /* !SIG_H_ */

commit d4250b2872b7dee58b8679346117675623812c31

sig: check parent process liveness with fd read
79f40f6 (sig: make PDEATHSIG handling non-racy) introduced a failure
mode that I hadn't forseen at the time; consider the following:

$ bst --pidfile=/tmp/pid sleep infinity &
$ bst --share all=/tmp/pid true

If you strace the second bst invocation, you'll see the following:

...
[pid 3475469] prctl(PR_SET_PDEATHSIG, SIGKILL) = 0
[pid 3475469] getppid() = 0
[pid 3475469] tgkill(1, 1, SIGKILL) = 0

Notice that getppid() returns 0 -- this happens because when entering an
existing pid namespace, the parent of a non-init process (which exists
outside of the namespace) gets replaced by 0. This sometimes confuses
processes (especially shells), but is for the most part fairly
innocent...

... except for the problem that 79f40f6 aimed to fix. The parent gets
its own pid with getpid(), but that value has no chance of being the
same as getppid() when the child is in the pid namespace. I'm not sure
how it even managed to work, but I think we got saved by init being able
to get an accurate value for getppid().

In any case, this was the wrong approach. A more reliable way of
detecting whether your parent died, is to simply arrange for said parent
to set up a nonblocking pipe, and do nothing with it. When reading from
the pipe in the child, one of two things can then happen: either the
read succeeds with EOF, meaning the pipe was closed by the kernel on
process death, or the read fails with EAGAIN, which means the process is
still alive.

enter.c
@@ -325,10 +325,18 @@ int enter(struct entry_settings *opts)
 		err(1, "socketpair");
 	}
 
-	/* Note that we need to get the parent pid here, as calling getppid()
-	   post-fork will not catch conditions where the parent gets SIGKILL'ed
-	   and the child ends up being reparented. */
-	pid_t expected_ppid = getpid();
+	/* Set up a pipe that we do nothing with; we use the read end as the parent
+	   handle in sig_setpdeathsig, and the write end will get closed on
+	   process exit. This ensures that we are able to properly detect process
+	   reparenting before we've called prctl(PR_SET_PDEATHSIG). */
+	enum {
+		LIVENESS_CHECK,
+		LIVENESS_KEEP,
+	};
+	int liveness_fds[2];
+	if (pipe2(liveness_fds, O_CLOEXEC | O_NONBLOCK) == -1) {
+		err(1, "pipe2");
+	}
 
 	/* You can't "really" unshare the PID namespace of a running process
 	   without forking, since for process hierarchy reasons only the next
@@ -361,6 +369,8 @@ int enter(struct entry_settings *opts)
 		}
 
 		close(socket_fdpass[SOCKET_CHILD]);
+		close(liveness_fds[LIVENESS_CHECK]);
+
 		if (opts->pidfile != NULL) {
 			int pidfile = open(opts->pidfile, O_WRONLY | O_CREAT | O_CLOEXEC | O_NOCTTY , 0666);
 			if (pidfile == -1) {
@@ -450,6 +460,8 @@ int enter(struct entry_settings *opts)
 		}
 	}
 
+	close(liveness_fds[LIVENESS_KEEP]);
+
 	/* err() and errx() cannot use exit(), since it's not fork-safe. */
 	err_exit = _exit;
 
@@ -466,7 +478,7 @@ int enter(struct entry_settings *opts)
 	   dies from uncatcheable signals. Or at least, we could, but this makes us
 	   leaky by default which isn't great, and the obvious workaround to
 	   daemonize the process tree is to just nohup bst. */
-	sig_setpdeathsig(SIGKILL, expected_ppid);
+	sig_setpdeathsig(SIGKILL, liveness_fds[LIVENESS_CHECK]);
 
 	outer_helper_sync(&outer_helper);
 

outer.c
@@ -315,10 +315,18 @@ void outer_helper_spawn(struct outer_helper *helper)
 		err(1, "outer_helper: socketpair");
 	}
 
-	/* Note that we need to get the parent pid here, as calling getppid()
-	   post-fork will not catch conditions where the parent gets SIGKILL'ed
-	   and the child ends up being reparented. */
-	pid_t expected_ppid = getpid();
+	/* Set up a pipe that we do nothing with; we use the read end as the parent
+	   handle in sig_setpdeathsig, and the write end will get closed on
+	   process exit. This ensures that we are able to properly detect process
+	   reparenting before we've called prctl(PR_SET_PDEATHSIG). */
+	enum {
+		LIVENESS_CHECK,
+		LIVENESS_KEEP,
+	};
+	int liveness_fds[2];
+	if (pipe2(liveness_fds, O_CLOEXEC | O_NONBLOCK) == -1) {
+		err(1, "pipe2");
+	}
 
 	pid_t pid = fork();
 	if (pid == -1) {
@@ -327,17 +335,18 @@ void outer_helper_spawn(struct outer_helper *helper)
 
 	if (pid) {
 		close(fdpair[SOCKET_CHILD]);
+		close(liveness_fds[LIVENESS_CHECK]);
 		helper->pid = pid;
 		helper->fd  = fdpair[SOCKET_PARENT];
 		return;
 	}
 
+	sig_setpdeathsig(SIGKILL, liveness_fds[LIVENESS_CHECK]);
+
 	/* Make sure all file descriptors except for the ones we're actually using
 	   get closed. This avoids keeping around file descriptors on which
 	   the parent process might be waiting on. */
 	rebind_fds_and_close_rest(3, &fdpair[SOCKET_CHILD], NULL);
-
-	sig_setpdeathsig(SIGKILL, expected_ppid);
 
 	sigset_t mask;
 	sigemptyset(&mask);

sig.c
@@ -77,15 +77,34 @@ void sig_setup(int epollfd, const sigset_t *set, pid_t helper_pid, epoll_handler
 	}
 }
 
-void sig_setpdeathsig(int signo, pid_t expected_ppid)
+void sig_setpdeathsig(int signo, int parent_handle)
 {
 	if (prctl(PR_SET_PDEATHSIG, signo) == -1) {
 		err(1, "prctl PR_SET_PDEATHSIG");
 	}
-	if (getppid() != expected_ppid) {
+
+	errno = 0;
+	int unused;
+	ssize_t ret = read(parent_handle, &unused, sizeof(unused));
+	if (ret == -1) {
+		ret = -errno;
+	}
+
+	switch (ret) {
+	case 0:
 		/* The parent process died unexpectedly and we got reparented to the
 		   nearest subreaper. We won't get killed by the kernel anymore, because
 		   our new parent might be long lived, so just do it ourselves. */
 		raise(signo);
+
+#if EWOULDBLOCK != EAGAIN
+	case -EWOULDBLOCK:
+#endif
+	case -EAGAIN:
+		/* The parent process is alive; continue on */
+		return;
+
+	default:
+		err(1, "sig_setpdeathsig: read");
 	}
 }

sig.h
@@ -51,6 +51,6 @@ void sig_wait(const sigset_t *set, siginfo_t *info);
 void sig_forward(const siginfo_t *info, pid_t pid);
 void sig_read(int sigfd, siginfo_t *info);
 void sig_setup(int epollfd, const sigset_t *set, pid_t helper_pid, epoll_handler_fn *fn);
-void sig_setpdeathsig(int signo, pid_t expected_ppid);
+void sig_setpdeathsig(int signo, int parent_handle);
 
 #endif /* !SIG_H_ */