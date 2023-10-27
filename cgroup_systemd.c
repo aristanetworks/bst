aristanetworks/bst

commit 343c4f1580386a9100a5c4b17bea8d0aed903df7

cgroups,systemd: garbage-collect failed invocations
By default, failed systemd units are kept around for inspection, but
this is not desirable for bst's own use-case, since `bst false` is a
valid invocation that should not warrant its own persistence.

cgroup_systemd.c
@@ -224,8 +224,9 @@ static int cgroup_systemd_join_cgroup(const char *parent, const char *name)
 	ok = ok && dbus_message_iter_open_container(&iter, DBUS_TYPE_ARRAY, "(sv)", &props);
 
 	ok = ok && bus_message_append(&props, "(sv)(sv)(sv)(sv)",
-			"Description", "s", "/usr/bin/true",
+			"Description", "s", "bst",
 			"Delegate", "b", 1, /* Delegate all cgroup controllers to us */
+			"CollectMode", "s", "inactive-or-failed", /* Make sure failed invocations are garbage-collected */
 			"Slice", "s", parent,
 			"PIDs", "au", 1, (uint32_t)getpid());
 

commit b679502f977fab127b66065f227178eb3a5ae144

cgroup,systemd: fix typo in dbus payload structure
343c4f1 ("cgroups,systemd: garbage-collect failed invocations")
introduced an error in the dbus message by forgetting to add an
additional key-value pair to the property list. This commit
fixes this error.

cgroup_systemd.c
@@ -223,7 +223,7 @@ static int cgroup_systemd_join_cgroup(const char *parent, const char *name)
 	/* Set properties */
 	ok = ok && dbus_message_iter_open_container(&iter, DBUS_TYPE_ARRAY, "(sv)", &props);
 
-	ok = ok && bus_message_append(&props, "(sv)(sv)(sv)(sv)",
+	ok = ok && bus_message_append(&props, "(sv)(sv)(sv)(sv)(sv)",
 			"Description", "s", "bst",
 			"Delegate", "b", 1, /* Delegate all cgroup controllers to us */
 			"CollectMode", "s", "inactive-or-failed", /* Make sure failed invocations are garbage-collected */