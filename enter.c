aristanetworks/bst

commit 1a6e29b6aba2bda8c627dfc1fd7a4f2ca5399b21

seccomp: disable mknod emulation by default
This commit disables the emulation of mknod and mknodat for safe
character devices, and introduces a flag to enable it again.

sec.c
@@ -30,6 +30,7 @@
 #include "util.h"
 
 int sec_seccomp_fix_stat_32bit = 0;
+int sec_seccomp_emulate_mknod = 0;
 
 typedef int syscall_handler_func(int, int, struct seccomp_notif *);
 
@@ -568,61 +569,13 @@ static int seccomp(unsigned int op, unsigned int flags, void *args)
 	return syscall(__NR_seccomp, op, flags, args);
 }
 
-int sec_seccomp_install_filter(void)
-{
-	struct sock_fprog prog = {
-		.len    = syscall_filter_length,
-		.filter = (struct sock_filter *)syscall_filter,
-	};
-
-	int fd = seccomp(SECCOMP_SET_MODE_FILTER, SECCOMP_FILTER_FLAG_NEW_LISTENER, &prog);
-	if (fd == -1) {
-		if (errno == EBUSY) {
-			// We're likely running bst in bst; ignore the error, and return
-			// a useless file descriptor to pass to the seccomp supervisor
-			return epoll_create1(EPOLL_CLOEXEC);
-		}
-		err(1, "seccomp SECCOMP_SET_MODE_FILTER");
-	}
-	return fd;
-}
+static syscall_handler_func *syscall_table[BST_NR_MAX+1];
+static syscall_handler_func *syscall_table_32[BST_NR_MAX32+1];
 
 static void sec_seccomp_dispatch_syscall(int seccomp_fd,
 		struct seccomp_notif *req,
 		struct seccomp_notif_resp *resp)
 {
-	static syscall_handler_func *const syscall_table[BST_NR_MAX+1] = {
-#ifdef BST_NR_mknod
-		[BST_NR_mknod]   = sec__mknod,
-#endif
-		[BST_NR_mknodat] = sec__mknodat,
-	};
-
-#ifdef BST_SECCOMP_32
-	syscall_handler_func *syscall_table_32[BST_NR_MAX32+1] = {
-#ifdef BST_NR_mknod_32
-		[BST_NR_mknod_32]   = sec__mknod,
-#endif
-		[BST_NR_mknodat_32] = sec__mknodat,
-	};
-
-	if (sec_seccomp_fix_stat_32bit) {
-#ifdef BST_NR_stat64_32
-		syscall_table_32[BST_NR_stat64_32] = sec__stat64;
-#endif
-#ifdef BST_NR_lstat64_32
-		syscall_table_32[BST_NR_lstat64_32] = sec__lstat64;
-#endif
-#ifdef BST_NR_fstat64_32
-		syscall_table_32[BST_NR_fstat64_32] = sec__fstat64;
-#endif
-#ifdef BST_NR_fstatat64_32
-		syscall_table_32[BST_NR_fstatat64_32] = sec__fstatat64;
-#endif
-		syscall_table_32[BST_NR_statx_32] = sec__statx;
-	}
-#endif
-
 	resp->id = req->id;
 
 	syscall_handler_func *const *table = syscall_table;
@@ -746,3 +699,53 @@ noreturn void sec_seccomp_supervisor(int seccomp_fd)
 	}
 }
 
+int sec_seccomp_install_filter(void)
+{
+	struct sock_fprog prog = {
+		.len    = syscall_filter_length,
+		.filter = (struct sock_filter *)syscall_filter,
+	};
+
+#ifdef BST_SECCOMP_32
+	if (sec_seccomp_fix_stat_32bit) {
+#ifdef BST_NR_stat64_32
+		syscall_table_32[BST_NR_stat64_32] = sec__stat64;
+#endif
+#ifdef BST_NR_lstat64_32
+		syscall_table_32[BST_NR_lstat64_32] = sec__lstat64;
+#endif
+#ifdef BST_NR_fstat64_32
+		syscall_table_32[BST_NR_fstat64_32] = sec__fstat64;
+#endif
+#ifdef BST_NR_fstatat64_32
+		syscall_table_32[BST_NR_fstatat64_32] = sec__fstatat64;
+#endif
+		syscall_table_32[BST_NR_statx_32] = sec__statx;
+	}
+#endif
+
+	if (sec_seccomp_emulate_mknod) {
+#ifdef BST_NR_mknod
+		syscall_table[BST_NR_mknod] = sec__mknod;
+#endif
+		syscall_table[BST_NR_mknodat] = sec__mknodat;
+
+#ifdef BST_SECCOMP_32
+#ifdef BST_NR_mknod_32
+		syscall_table_32[BST_NR_mknod_32] = sec__mknod;
+#endif
+		syscall_table_32[BST_NR_mknodat_32] = sec__mknodat;
+#endif
+	}
+
+	int fd = seccomp(SECCOMP_SET_MODE_FILTER, SECCOMP_FILTER_FLAG_NEW_LISTENER, &prog);
+	if (fd == -1) {
+		if (errno == EBUSY) {
+			// We're likely running bst in bst; ignore the error, and return
+			// a useless file descriptor to pass to the seccomp supervisor
+			return epoll_create1(EPOLL_CLOEXEC);
+		}
+		err(1, "seccomp SECCOMP_SET_MODE_FILTER");
+	}
+	return fd;
+}

main.c
@@ -67,6 +67,7 @@ enum {
 
 	/* Opt-in feature flags */
 	OPTION_FIX_STAT_32BIT_OVERFLOW,
+	OPTION_EMULATE_MKNOD,
 
 	/* Opt-out feature flags */
 	OPTION_NO_FAKE_DEVTMPFS,
@@ -395,6 +396,7 @@ int main(int argc, char *argv[], char *envp[])
 		/* Opt-in feature flags */
 #ifdef HAVE_SECCOMP_UNOTIFY
 		{ "fix-stat-32bit-overflow", no_argument, NULL, OPTION_FIX_STAT_32BIT_OVERFLOW },
+		{ "emulate-mknod",           no_argument, NULL, OPTION_EMULATE_MKNOD           },
 #endif
 
 		/* Opt-out feature flags */
@@ -867,6 +869,12 @@ int main(int argc, char *argv[], char *envp[])
 			case OPTION_FIX_STAT_32BIT_OVERFLOW:
 			{
 				sec_seccomp_fix_stat_32bit = 1;
+				break;
+			}
+
+			case OPTION_EMULATE_MKNOD:
+			{
+				sec_seccomp_emulate_mknod = 1;
 				break;
 			}
 #endif

man/bst.1.scd
@@ -410,6 +410,23 @@ spacetime process.
 
 	This flag has no effect on programs running with a 64-bit personality.
 
+\--emulate-safe-mknod
+	Emulate the mknod system call in user programs for safe devices.
+
+	If turned on, this will cause inner programs to successfully use the mknod
+	system call to create character devices deemed safe.
+
+	Character devices that are safe to create, along with their (major, minor)
+	device number pair, are:
+
+	- *whiteout* (0, 0)
+	- *null* (1, 3)
+	- *zero* (1, 5)
+	- *full* (1, 7)
+	- *random* (1, 8)
+	- *urandom* (1, 9)
+	- *tty* (5, 0)
+
 \--no-copy-hard-rlimits
 	Do not copy hard limit values to soft limits for all resources mentioned above.
 

sec.h
@@ -13,5 +13,6 @@ int sec_seccomp_install_filter(void);
 noreturn void sec_seccomp_supervisor(int);
 
 extern int sec_seccomp_fix_stat_32bit;
+extern int sec_seccomp_emulate_mknod;
 
 #endif /* !SEC_H_ */

usage.c
@@ -353,6 +353,19 @@ unsigned char usage_txt[] = {
   0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
   0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
   0x20, 0x20, 0x20, 0x20, 0x20, 0x65, 0x78, 0x65, 0x63, 0x75, 0x74, 0x61,
-  0x62, 0x6c, 0x65, 0x2e, 0x0a
+  0x62, 0x6c, 0x65, 0x2e, 0x0a, 0x0a, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+  0x2d, 0x2d, 0x66, 0x69, 0x78, 0x2d, 0x73, 0x74, 0x61, 0x74, 0x2d, 0x33,
+  0x32, 0x62, 0x69, 0x74, 0x2d, 0x6f, 0x76, 0x65, 0x72, 0x66, 0x6c, 0x6f,
+  0x77, 0x20, 0x20, 0x20, 0x20, 0x20, 0x4d, 0x61, 0x6b, 0x65, 0x20, 0x33,
+  0x32, 0x2d, 0x62, 0x69, 0x74, 0x20, 0x73, 0x74, 0x61, 0x74, 0x20, 0x77,
+  0x6f, 0x72, 0x6b, 0x20, 0x6f, 0x6e, 0x20, 0x36, 0x34, 0x2d, 0x62, 0x69,
+  0x74, 0x20, 0x66, 0x69, 0x6c, 0x65, 0x73, 0x79, 0x73, 0x74, 0x65, 0x6d,
+  0x73, 0x2e, 0x0a, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x2d, 0x2d, 0x65,
+  0x6d, 0x75, 0x6c, 0x61, 0x74, 0x65, 0x2d, 0x6d, 0x6b, 0x6e, 0x6f, 0x64,
+  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x45, 0x6d, 0x75, 0x6c, 0x61, 0x74, 0x65, 0x20, 0x6d,
+  0x6b, 0x6e, 0x6f, 0x64, 0x20, 0x66, 0x6f, 0x72, 0x20, 0x73, 0x61, 0x66,
+  0x65, 0x20, 0x63, 0x68, 0x61, 0x72, 0x61, 0x63, 0x74, 0x65, 0x72, 0x20,
+  0x64, 0x65, 0x76, 0x69, 0x63, 0x65, 0x73, 0x2e, 0x0a
 };
-unsigned int usage_txt_len = 4169;
+unsigned int usage_txt_len = 4329;

commit 4003e9f5a41cb89fef823d61247db92e885185db

seccomp: move syscall table initialization to supervisor
1a6e29b ("seccomp: disable mknod emulation by default") introduced a
regression by moving the seccomp syscall table initialization to the
process that installs the filter. This however is not the process in
which the supervisor runs in, meaning the syscall table was left empty,
and caused all of the custom logic to be skipped.

enter.c
@@ -469,11 +469,11 @@ int enter(struct entry_settings *opts)
 	ns_enter_postfork(namespaces, ns_len);
 
 #ifdef HAVE_SECCOMP_UNOTIFY
-		int seccomp_fd = sec_seccomp_install_filter();
-		send_fd(outer_helper.fd, seccomp_fd);
-		if (seccomp_fd != -1) {
-			close(seccomp_fd);
-		}
+	int seccomp_fd = sec_seccomp_install_filter();
+	send_fd(outer_helper.fd, seccomp_fd);
+	if (seccomp_fd != -1) {
+		close(seccomp_fd);
+	}
 #endif
 
 	outer_helper_close(&outer_helper);

sec.c
@@ -597,6 +597,7 @@ static void sec_seccomp_dispatch_syscall(int seccomp_fd,
 #endif
 
 	if (req->data.nr <= 0 || (size_t) req->data.nr >= nr_syscall) {
+		warnx("BUG: intercepted syscall %d on arch %x, but it was not in the supervisor's syscall table.", req->data.nr, req->data.arch);
 		goto fallthrough;
 	}
 	syscall_handler_func *fn = table[(size_t) req->data.nr];
@@ -643,6 +644,40 @@ fallthrough:
 
 noreturn void sec_seccomp_supervisor(int seccomp_fd)
 {
+	/* Populate any syscall handler tables prior to running the supervisor */
+
+#ifdef BST_SECCOMP_32
+	if (sec_seccomp_fix_stat_32bit) {
+#ifdef BST_NR_stat64_32
+		syscall_table_32[BST_NR_stat64_32] = sec__stat64;
+#endif
+#ifdef BST_NR_lstat64_32
+		syscall_table_32[BST_NR_lstat64_32] = sec__lstat64;
+#endif
+#ifdef BST_NR_fstat64_32
+		syscall_table_32[BST_NR_fstat64_32] = sec__fstat64;
+#endif
+#ifdef BST_NR_fstatat64_32
+		syscall_table_32[BST_NR_fstatat64_32] = sec__fstatat64;
+#endif
+		syscall_table_32[BST_NR_statx_32] = sec__statx;
+	}
+#endif
+
+	if (sec_seccomp_emulate_mknod) {
+#ifdef BST_NR_mknod
+		syscall_table[BST_NR_mknod] = sec__mknod;
+#endif
+		syscall_table[BST_NR_mknodat] = sec__mknodat;
+
+#ifdef BST_SECCOMP_32
+#ifdef BST_NR_mknod_32
+		syscall_table_32[BST_NR_mknod_32] = sec__mknod;
+#endif
+		syscall_table_32[BST_NR_mknodat_32] = sec__mknodat;
+#endif
+	}
+
 	/* Run the seccomp supervisor. This supervisor is a privileged helper
 	   that runs safe syscalls on behalf of the unprivileged child in a
 	   user namespace.
@@ -710,38 +745,6 @@ int sec_seccomp_install_filter(void)
 		.len    = syscall_filter_length,
 		.filter = (struct sock_filter *)syscall_filter,
 	};
-
-#ifdef BST_SECCOMP_32
-	if (sec_seccomp_fix_stat_32bit) {
-#ifdef BST_NR_stat64_32
-		syscall_table_32[BST_NR_stat64_32] = sec__stat64;
-#endif
-#ifdef BST_NR_lstat64_32
-		syscall_table_32[BST_NR_lstat64_32] = sec__lstat64;
-#endif
-#ifdef BST_NR_fstat64_32
-		syscall_table_32[BST_NR_fstat64_32] = sec__fstat64;
-#endif
-#ifdef BST_NR_fstatat64_32
-		syscall_table_32[BST_NR_fstatat64_32] = sec__fstatat64;
-#endif
-		syscall_table_32[BST_NR_statx_32] = sec__statx;
-	}
-#endif
-
-	if (sec_seccomp_emulate_mknod) {
-#ifdef BST_NR_mknod
-		syscall_table[BST_NR_mknod] = sec__mknod;
-#endif
-		syscall_table[BST_NR_mknodat] = sec__mknodat;
-
-#ifdef BST_SECCOMP_32
-#ifdef BST_NR_mknod_32
-		syscall_table_32[BST_NR_mknod_32] = sec__mknod;
-#endif
-		syscall_table_32[BST_NR_mknodat_32] = sec__mknodat;
-#endif
-	}
 
 	int fd = seccomp(SECCOMP_SET_MODE_FILTER, SECCOMP_FILTER_FLAG_NEW_LISTENER, &prog);
 	if (fd == -1) {